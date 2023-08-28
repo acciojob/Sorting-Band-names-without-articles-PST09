19dac75f3c5a681905e36ee4a0a23af299548be7
Priyanka Singh Tomer <136883301+PST09@users.noreply.github.com>
2023-08-28 15:29:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-//your code here
-
+const bands = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'Aerosmith'];
+function strip(word) {
+const regex = /^(a |the |an )/i;
+return word.replace(regex, '').trim();
+}
+const sortedBands = bands.sort((a, b) => (strip(a) > strip(b)) ? 1 : -1);
+const bandList = document.getElementById('band');
+sortedBands.forEach(band => {
+const listItem = document.createElement('li');
+listItem.textContent = band;
+bandList.appendChild(listItem);
+});
